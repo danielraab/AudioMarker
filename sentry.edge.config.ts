66cf72bdfd6c5896a4cefdaadbdf7c8f9b3ab727
Daniel Raab <daniel@draab.at>
2026-02-02 15:48:00 +0000

    try to fix sentry logging

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,4 +6,10 @@ Sentry.init({
   environment: process.env.NODE_ENV,
   tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE ?? 0.1),
   profilesSampleRate: Number(process.env.SENTRY_PROFILES_SAMPLE_RATE ?? 0),
+  integrations: [
+    // send console.log, console.warn, and console.error calls as logs to Sentry
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 });
